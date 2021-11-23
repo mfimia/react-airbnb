69e9f389a3717912382cc133ac20576a3c931c32
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-23 13:09:30 +0100

    updated airbnb logo

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import Navbar from './Navbar.js'
+export default function App() {
+  return (
+     <Navbar />
+  );
+}
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+import "./Navbar.css";
+import airbnbLogo from "./img/airbnb-transparent.png";
+export default function Navbar() {
+  return (
+    <nav>
+      <img alt="Airnbnb logo" src={airbnbLogo} />
+    </nav>
+  );
+}
