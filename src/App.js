055c1bd1a9ef7d5cba81039a1cb7d39ec4dbd67e
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-23 18:47:45 +0100

    added contact card props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import Navbar from "./Navbar.js";
-import MainSection from "./MainSection";
-import "./css/index.css";
+// import Navbar from "./Navbar.js";
+// import MainSection from "./MainSection";
+// import "./css/index.css";
+import Contact from "./Contact.js";
+// import './Contact.css'
 export default function App() {
+  //   return (
+  //     <div className="App">
+  //       <Navbar />
+  //       <MainSection />
+  //     </div>
+  //   );
   return (
-    <div className="App">
-      <Navbar />
-      <MainSection />
+    <div className="contacts">
+      <Contact
+        img="./img/image 12.png"
+        name="Lukitas"
+        phone="+00nope"
+        email="nope@nope.nop"
+      />
+      <Contact
+        img="./img/image 23.png"
+        name="Toni"
+        phone="+00nope"
+        email="nope@nope.nop"
+      />
+      <Contact
+        img="./img/image 29.png"
+        name="Suso"
+        phone="+00nope"
+        email="nope@nope.nop"
+      />
+      <Contact
+        img="./img/image 28.png"
+        name="Siso"
+        phone="+00nope"
+        email="nope@nope.nop"
+      />
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,20 @@
+import "./Contact.css";
+// import pic from "./img/image 12.png";
+export default function Contact(props) {
+  return (
+    <>
+      <div className="contact-card">
+        <img src={props.img} alt="" />
+        <h3>{props.name}</h3>
+        <div className="info-group">
+          <img src={props.img} alt="" />
+          <p>{props.phone}</p>
+        </div>
+        <div className="info-group">
+          <img src={props.img} alt="" />
+          <p>{props.email}</p>
+        </div>
+      </div>
+    </>
+  );
+}
